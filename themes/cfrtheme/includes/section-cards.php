176f8ffdbf2f5eb3e95bf785453316a09a74434b
Slip2188 <ritika.r.gaikwad@gmail.com>
2021-08-18 10:41:49 +0530

    Added Cards Section Background

--- themes/cfrtheme/includes/section-cards.php
+++ themes/cfrtheme/includes/section-cards.php
@@ -1,12 +1,62 @@
 <section>
   <div class="cards-section-container container-fluid">
 
+    <img src="<?php echo get_template_directory_uri();?>/images/card-icons/cards-border.svg" class='cards-bg-border'>
+    <img src="<?php echo get_template_directory_uri();?>/images/card-icons/cfr-scrabble.svg" class="cards-bg-scrabble">
+    <img src="<?php echo get_template_directory_uri();?>/images/card-icons/discs.svg" class="cards-bg-discs">  
+    <img src="<?php echo get_template_directory_uri();?>/images/card-icons/magic-5-ball.svg" class="cards-bg-magic-5">
+    <img src="<?php echo get_template_directory_uri();?>/images/card-icons/stationary.svg" class="cards-bg-stationary">  
+    <img src="<?php echo get_template_directory_uri();?>/images/card-icons/dices.svg" class="cards-bg-dices">
+
+
     <div class="heading-container">
     <?php
       $post = get_posts( array( "category_name" => "Cards Post") )[0];
-      echo "<div class='heading-overlay'>" . $post->post_title . "</div>";
+      echo "<div class='heading-overlay cards-heading'>" . $post->post_title . "</div>";
     ?>
+    
     </div>
+
+    
+
+    
+    <div class="cards-body row">
+        <?php
+        // $doc = new DOMDocument();
+        // $doc->loadHTML(apply_filters( 'the_content', $post->post_content ));
+        // $doc = new DOMXPath($doc);
+
+        // $i = 0;
+        // $cards = $doc->query("//p");
+        // foreach ( $cards as $card) {
+        //   if ($i == intdiv(sizeof($cards), 3)) {
+        //     echo "</div>";
+        //     echo "<div class='col-md-3 card'>";
+        //   }
+        //   else if ($i == intdiv(sizeof($cards), 4)){
+        //     echo "</div>";
+        //     echo "<div class='col-md-3 card'>";
+        //   }
+        //   else {
+        //     echo "</div>";
+        //     echo "<div class='col-md-3 card'>";
+        //   }
+
+        //   if ($i % 3 == 0) { // Card Name
+        //     echo $card->nodeValue;
+        //   }
+        //   else { // Card Content
+          
+        //     echo $card->nodeValue;
+           
+        //   }
+
+        //   $i += 1;
+        // }
+        ?>
+    </div>
+
+
 
   </div>
 </section>
--- themes/cfrtheme/includes/section-home-post1.php
+++ themes/cfrtheme/includes/section-home-post1.php
@@ -2,7 +2,7 @@
   <div class="home-post container-fluid">
     <div class="heading-container">
     <?php
-      $post = get_posts( array( "category_name" => "Home ost 1") )[0];
+      $post = get_posts( array( "category_name" => "Home Post 1") )[0];
       echo "<div class='heading-overlay'>" . $post->post_title . "</div>";
       echo "<div class='heading-watermark'>" . get_the_tags($post->ID)[0]->name . "</div>";
     ?>
