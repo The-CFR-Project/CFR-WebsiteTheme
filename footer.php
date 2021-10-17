7c9f8fa4e51c9b86029a172d2f2ace92ad25d633
Siddhant Kaul <91250416+siddkaul@users.noreply.github.com>
2021-10-17 09:50:19 +0530

    Formatting
    Use tabs instead of irregular spaces

--- footer.php
+++ footer.php
@@ -3,41 +3,41 @@
 <?php wp_footer();?>
 <style type="text/css">   @import url("<?php echo get_template_directory_uri(); ?>/css/homepage-css/footer.css"); </style>
 <div id="footer-container">
-  <div class="full-search-container">
+	<div class="full-search-container">
 
-    <!-- Load icon library -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<!-- Load icon library -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <form action="action_page.php">
-      <input type="text" placeholder="Search the CFR website..." name="search">
-      <button type="submit">
-	 <img src="<?php echo get_template_directory_uri();?>/images/search.svg">
-      </button>
-    </form>
+		<form action="action_page.php">
+			<input type="text" placeholder="Search the CFR website..." name="search">
+			<button type="submit">
+				<img src="<?php echo get_template_directory_uri();?>/images/search.svg">
+			</button>
+		</form>
 
-  </div>
+	</div>
 
-  <?php
-    wp_nav_menu(
-      array(
-	 "theme_location" => "footer",
-	 "menu_class" => "footer"
-      )
-    );
-  ?>
+	<?php
+	wp_nav_menu(
+		array(
+		"theme_location" => "footer",
+		"menu_class" => "footer"
+		)
+		);
+	?>
 
-  <div id="footer-social-bar">
-    <?php
-    $socialItems = wp_get_nav_menu_items(get_nav_menu_locations()['footer-social']);
-    foreach ( $socialItems as $footerItem ) {
-      echo "<a href='" . $footerItem->url . "'><img src='" . get_template_directory_uri() . "/images/" . $footerItem->title . "-icon.svg'></a>";
-    }
-    ?>
-  </div>
+	<div id="footer-social-bar">
+		<?php
+			$socialItems = wp_get_nav_menu_items(get_nav_menu_locations()['footer-social']);
+			foreach ( $socialItems as $footerItem ) {
+			  echo "<a href='" . $footerItem->url . "'><img src='" . get_template_directory_uri() . "/images/" . $footerItem->title . "-icon.svg'></a>";
+			}
+		?>
+	</div>
 
-  <div id="copyright-bar">
-    <small>© The Carbon Footprint Reduction Project</small>
-  </div>
+	<div id="copyright-bar">
+		<small>© The Carbon Footprint Reduction Project</small>
+	</div>
 
 </div>
 
--- header.php
+++ header.php
@@ -1,47 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>The CFR Project</title>
+	<meta charset="UTF-8">
+	<title>The CFR Project</title>
 
-    <?php wp_head();?>
+	<?php wp_head();?>
 
 </head>
 <body onscroll="document.getElementById('sidenav').className='hide collapse';">
 <style type="text/css">   @import url("<?php echo get_template_directory_uri(); ?>/css/homepage-css/header.css"); </style>
 <header id = "topnav">
-  <div class="nav-header-container">
-    <div class="nav-logo-container">
+	<div class="nav-header-container">
+		<div class="nav-logo-container">
 
-      <?php
-        $custom_logo_id = get_theme_mod( 'custom_logo' );
-        $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
-      ?>
+		  <?php
+			$custom_logo_id = get_theme_mod( 'custom_logo' );
+			$image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+		  ?>
 
-      <a href="<?php echo home_url();?>">
-        <img class="logo" src="<?php echo $image[0];?>" alt="">
-      </a>
+		  <a href="<?php echo home_url();?>">
+			<img class="logo" src="<?php echo $image[0];?>" alt="">
+		  </a>
 
-    </div>
+		</div>
 
-    <div class="nav-title-container">
+		<div class="nav-title-container">
 
-      <a href="<?php echo home_url();?>">
-        <h1 class="green1"  id = "navbar-title">The Carbon Footprint Reduction Project</h1>
-      </a>
+		  <a href="<?php echo home_url();?>">
+			<h1 class="green1"  id = "navbar-title">The Carbon Footprint Reduction Project</h1>
+		  </a>
 
-    </div>
-  </div>
+		</div>
+	</div>
 
   <div class="nav-container">
-    <?php
-      wp_nav_menu(
-        array(
-          "theme_location" => "nav-bar",
-          "menu_class" => "nav-bar"
-        )
-      );
-    ?>
+	<?php
+	  wp_nav_menu(
+		array(
+		  "theme_location" => "nav-bar",
+		  "menu_class" => "nav-bar"
+		)
+	  );
+	?>
   </div>
 
 
