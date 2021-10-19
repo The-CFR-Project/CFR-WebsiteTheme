3e60a15503ad40b28bf60b3af9f330c4655b8173
Bhavye Mathur <bhavyemathur@gmail.com>
2021-10-19 21:16:11 +0530

    DB test

--- category-blog.php
+++ category-blog.php
@@ -7,6 +7,14 @@ Template Name: Blog
 
 <?php get_header();?>
 
+<?php
+global $wpdb;
+$results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}users", OBJECT );
+foreach ($results as $result) {
+  print_r($result);
+}
+?>
+
 <?php get_template_part( "includes/blogs-archive/section", "blogs-carousel" );?>
 <?php get_template_part( "includes/blogs-archive/section", "archive" );?>
 <?php get_template_part( "includes/blogs-archive/section", "blog-series-archive" );?>
