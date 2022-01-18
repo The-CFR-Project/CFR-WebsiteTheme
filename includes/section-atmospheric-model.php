212ffe2a4997f9662b3b13a360b4802ec77c8c73
Slip2188 <ritika.r.gaikwad@gmail.com>
2022-01-18 18:09:17 +0530

    Take Inputs for AMP

--- includes/section-atmospheric-model.php
+++ includes/section-atmospheric-model.php
@@ -43,12 +43,12 @@
     </div>
 
     <div class="inputs row">
-      <input type="date" name="date" class="col-md-3">
-      <input type="time" name="time" class="col-md-3">
-      <input type="text" name="altitude" placeholder="___ m" class="col-md-3">
-      <select class="col-md-3" name="model">
-        <option value="">pressure</option>
-        <option value="">temperature</option>
+      <input type="date" name="date" class="col-md-3" id="date">
+      <input type="time" name="time" class="col-md-3" id="time">
+      <input type="text" name="altitude" placeholder="___ m" class="col-md-3" id="altitude">
+      <select class="col-md-3" name="model" id="model">
+        <option value="pressure">pressure</option>
+        <option value="temperature">temperature</option>
       </select>
     </div>
 
@@ -56,11 +56,12 @@
 
   <div class="rendering-map">
     <div class="amp-map-container">
-      <svg id="amp-map-svg" width="800" height="450"></svg>
       <div id="background"></div>
+      <svg id="amp-map-svg" width="800" height="450"></svg>
     </div>
     <div class="altitude-slider-container">
-      <input type="range" min="0" max="18000" value="50" class="altitude-slider" id="myRange">
+      <input type="range" min="0" max="18000" step="10" value="100" class="altitude-slider" id="myRange">
+      <input type="number" value="100" min="0" max="18000"/>
     </div>
   </div>
 
@@ -70,98 +71,5 @@
   <script src="https://d3js.org/d3.v4.js"></script>
   <script src="https://d3js.org/d3-scale-chromatic.v1.min.js"></script>
   <script src="https://d3js.org/d3-geo-projection.v2.min.js"></script>
-  <script type="module">
-    function drawMapSVG() {
-      // The svg
-      const svg = d3.select("svg"),
-          width = +svg.attr("width"),
-          height = +svg.attr("height");
-
-      // Map and projection
-      const projection = d3.geoCylindricalStereographic()
-          .scale(width / 2.5 / Math.PI);
-          // .translate([width / 10, height / 10]);
-
-      // Load external data and boot
-      d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson", function(data){
-
-          // Draw the map
-          const features = '<?php echo get_template_directory_uri(); ?>/js/features.json';
-          svg.append("g")
-              .selectAll("path")
-              .data(data.features)
-              .enter().append("path")
-                  .attr("fill", "#004c65")
-                  .attr("d", d3.geoPath()
-                      .projection(projection)
-                  )
-                  .style("stroke", "#f0f0f0")
-      });
-
-    }
-
-    drawMapSVG();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    function runBackupCode(){
-      const geojson = '<?php echo get_template_directory_uri();?>/js/world-110m.json';
-      const projection = d3.geoEquirectangular();
-      const geoGenerator = d3.geoPath()
-                            .projection(projection);
-      let canvas = d3.select('#amp-map')
-               .selectAll('path')
-               .attr('d', geoGenerator)
-               .data(geojson.objects)
-               .join('path');
-      var context = canvas.getContext("2d");
-      context.beginPath();
-    }
-
-    function drawMapCanvas() {
-      var canvas = document.getElementById('amp-map');
-
-      var width = canvas.offsetWidth;
-      var height = canvas.offsetHeight;
-
-      var projection = d3.geoEquirectangular()
-          .scale(width / 1.3 / Math.PI)
-          .translate([width / 2, height / 2]);
-
-      var ctx = canvas.getContext('2d');
-
-      const pathGenerator = d3.geoPath(projection, ctx);
-
-      d3.json('<?php echo get_template_directory_uri();?>/js/world-110m.json', function(data){
-
-        // initialize the path
-        ctx.beginPath();
-
-        // Got the positions of the path
-        pathGenerator(data.objects);
-
-        // Fill the paths
-        ctx.fillStyle = "#999";
-        ctx.fill();
-
-        // Add stroke
-        ctx.strokeStyle = "#69b3a2";
-        ctx.stroke();
-      })
-    }
-
-  </script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/amp-js.js"></script>
 </section>
