f0a4af087858a03571c44fce9ca089e5c4331003
Slip2188 <ritika.r.gaikwad@gmail.com>
2022-01-19 22:39:19 +0530

    Add About the Gameshow Section to The Gameshow Page

---
+++ template-gameshow-page.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Template Name: Gameshow Page
+*/
+?>
+<?php get_header(); ?>
+
+<?php get_template_part( "includes/gameshow-page/section", "about-gameshow" );?>
+<?php get_template_part( "includes/gameshow-page/section", "gs-faqs" );?>
+
+<?php get_footer(); ?>
