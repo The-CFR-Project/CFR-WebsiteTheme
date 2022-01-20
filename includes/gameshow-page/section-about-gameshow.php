04ceb13ef779f21e9580443d076fbcea1eb11e16
Slip2188 <ritika.r.gaikwad@gmail.com>
2022-01-20 12:46:11 +0530

    Finish About Gameshow Section

--- includes/gameshow-page/section-about-gameshow.php
+++ includes/gameshow-page/section-about-gameshow.php
@@ -5,11 +5,11 @@ $doc->loadHTML( apply_filters( 'the_content', $post->post_content ) );
 $doc = new DOMXPath( $doc );
 
 $trailerLink = $doc->query('//a')[0]->nodeValue;
-$heading = $doc->query('//h1')[0]->nodeValue;
 $description = $doc->query('//h3')[0]->nodeValue;
-$buttonText = $doc->query('//p')[0]->nodeValue;
 ?>
+<section>
 
+</section>
 <section id="gameshow-about">
 <style type="text/css"> @import url("<?php echo get_template_directory_uri(); ?>/css/gameshow-page-css/about-gameshow.css"); </style>
 <div class="row">
@@ -18,7 +18,6 @@ $buttonText = $doc->query('//p')[0]->nodeValue;
         <iframe width="560" height="315" src="https://www.youtube.com/embed/XqZsoesa55w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>    
     </div>
     <div class="col-md-6 about-gameshow-container">
-        <h1><?php echo $heading; ?></h1>
         <p><?php echo $description; ?></p>
     </div>
 </div>
