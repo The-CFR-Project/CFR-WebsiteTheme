1a87d342b48a46ec0f357069382d8431f35a0dbb
Slip2188 <ritika.r.gaikwad@gmail.com>
2022-01-22 20:54:23 +0530

    Make dynamic meet the team section

--- includes/section-meet-the-team.php
+++ includes/section-meet-the-team.php
@@ -19,173 +19,49 @@ $doc = new DOMXPath( $doc );
     $roles = $doc->query("//h3");
     $imgs = $doc->query("//p");
     ?>
+    <?php for ($i=0; $i<=(count($names) - 1); $i++): ?>
 
-      <div class="row large-row">
+      <?php if($i == 0): echo '<div class="row large-row">'; endif; ?>
+        <?php if($i <= 2): ?>
         <div class="col-md-3">
           <div>
-            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/' . $imgs[$i]->nodeValue;?>"></div>
+            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/member-pics/' . $imgs[$i]->nodeValue;?>"></div>
             <div>
               <div><h6><?php echo $names[$i]->nodeValue?></h6></div>
               <div><p><?php echo $roles[$i]->nodeValue?></p></div>
             </div>
-            <?php $i++;?>
           </div>
         </div>
+        <?php endif; ?>
+      <?php if ($i == 2): echo '</div>'; endif; ?>
 
+      <?php if($i == 3): echo '<div class="row medium-row">'; endif; ?>
+        <?php if($i >= 3 && $i <= 5): ?>
         <div class="col-md-3">
           <div>
-            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/' . $imgs[$i]->nodeValue;?>"></div>
+            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/member-pics/' . $imgs[$i]->nodeValue;?>"></div>
             <div>
               <div><h6><?php echo $names[$i]->nodeValue?></h6></div>
               <div><p><?php echo $roles[$i]->nodeValue?></p></div>
             </div>
-            <?php $i++;?>
           </div>
         </div>
+        <?php endif; ?>
+      <?php if ($i == 5): echo '</div>'; endif; ?>
 
-        <div class="col-md-3">
-          <div>
-            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/' . $imgs[$i]->nodeValue;?>"></div>
-            <div>
-              <div><h6><?php echo $names[$i]->nodeValue?></h6></div>
-              <div><p><?php echo $roles[$i]->nodeValue?></p></div>
-            </div>
-            <?php $i++;?>
-          </div>
-        </div>
-      </div>
-
-      <div class="row medium-row">
-        <div class="col-md-3">
-          <div>
-            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/' . $imgs[$i]->nodeValue;?>"></div>
-            <div>
-              <div><h6><?php echo $names[$i]->nodeValue?></h6></div>
-              <div><p><?php echo $roles[$i]->nodeValue?></p></div>
-            </div>
-            <?php $i++;?>
-          </div>
-        </div>
-
-        <div class="col-md-3">
-          <div>
-            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/' . $imgs[$i]->nodeValue;?>"></div>
-            <div>
-              <div><h6><?php echo $names[$i]->nodeValue?></h6></div>
-              <div><p><?php echo $roles[$i]->nodeValue?></p></div>
-            </div>
-            <?php $i++;?>
-          </div>
-        </div>
-
-        <div class="col-md-3">
-          <div>
-            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/' . $imgs[$i]->nodeValue;?>"></div>
-            <div>
-              <div><h6><?php echo $names[$i]->nodeValue?></h6></div>
-              <div><p><?php echo $roles[$i]->nodeValue?></p></div>
-            </div>
-            <?php $i++;?>
-          </div>
-        </div>
-
-      </div>
-
-      <div class="row small-row">
+      
+      <?php if ($i == 6): echo '<div class="row small-row">'; endif; ?>
+        <?php if($i >= 6): ?>
         <div class="col-md-2">
           <div>
-            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/' . $imgs[$i]->nodeValue;?>"></div>
+            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/member-pics/' . $imgs[$i]->nodeValue;?>"></div>
             <div>
               <div><h6><?php echo $names[$i]->nodeValue?></h6></div>
             </div>
-            <?php $i++;?>
           </div>
         </div>
-
-        <div class="col-md-2">
-          <div>
-            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/' . $imgs[$i]->nodeValue;?>"></div>
-            <div>
-              <div><h6><?php echo $names[$i]->nodeValue?></h6></div>
-            </div>
-            <?php $i++;?>
-          </div>
-        </div>
-
-        <div class="col-md-2">
-          <div>
-            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/' . $imgs[$i]->nodeValue;?>"></div>
-            <div>
-              <div><h6><?php echo $names[$i]->nodeValue?></h6></div>
-            </div>
-            <?php $i++;?>
-          </div>
-        </div>
-
-        <div class="col-md-2">
-          <div>
-            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/' . $imgs[$i]->nodeValue;?>"></div>
-            <div>
-              <div><h6><?php echo $names[$i]->nodeValue?></h6></div>
-            </div>
-            <?php $i++;?>
-          </div>
-        </div>
-
-        <div class="col-md-2">
-          <div>
-            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/' . $imgs[$i]->nodeValue;?>"></div>
-            <div>
-              <div><h6><?php echo $names[$i]->nodeValue?></h6></div>
-            </div>
-            <?php $i++;?>
-          </div>
-        </div>
-
-      </div>
-
-      <div class="row small-row">
-        <div class="col-md-2">
-          <div>
-            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/' . $imgs[$i]->nodeValue;?>"></div>
-            <div>
-              <div><h6><?php echo $names[$i]->nodeValue?></h6></div>
-            </div>
-            <?php $i++;?>
-          </div>
-        </div>
-
-        <div class="col-md-2">
-          <div>
-            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/' . $imgs[$i]->nodeValue;?>"></div>
-            <div>
-              <div><h6><?php echo $names[$i]->nodeValue?></h6></div>
-            </div>
-            <?php $i++;?>
-          </div>
-        </div>
-
-        <div class="col-md-2">
-          <div>
-            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/' . $imgs[$i]->nodeValue;?>"></div>
-            <div>
-              <div><h6><?php echo $names[$i]->nodeValue?></h6></div>
-            </div>
-            <?php $i++;?>
-          </div>
-        </div>
-
-        <div class="col-md-2">
-          <div>
-            <div><img class="dp" src="<?php echo get_template_directory_uri() . '/images/' . $imgs[$i]->nodeValue;?>"></div>
-            <div>
-              <div><h6><?php echo $names[$i]->nodeValue?></h6></div>
-            </div>
-            <?php $i++;?>
-          </div>
-        </div>
-
-      </div>
-
+        <?php endif; ?>
+      <?php if ($i == (count($names)-1)): echo "</div>"; endif; ?>
+    <?php endfor; ?>  
   </div>
 </section>
