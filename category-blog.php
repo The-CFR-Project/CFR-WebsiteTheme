d677f821c4490bf87001d734ff9c4160baf80746
Slip2188 <ritika.r.gaikwad@gmail.com>
2022-01-24 19:12:48 +0530

    Polish Blogs Single Page

--- category-blog.php
+++ category-blog.php
@@ -17,6 +17,7 @@ Template Name: Blog
 
 <?php get_template_part( "includes/blogs-archive/section", "blogs-carousel" );?>
 <?php get_template_part( "includes/blogs-archive/section", "archive" );?>
+<?php get_template_part( "includes/blogs-archive/section", "blog-series-archive" );?>
 <?php get_template_part( "includes/section", "instawall" );?>
 
 <?php get_footer();?>
