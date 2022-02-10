02b35290ac70b252693f7721674e9c29f857cf08
BalloonInTheVoid <ballooninthevoid@gmail.com>
2022-02-10 16:12:01 +0530

    Super minor change in header.php

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="Cache-control" content="no-cache">
+	<!-- <meta http-equiv="Cache-control" content="no-cache"> -->
 	<title>The CFR Project</title>
 
 	<?php wp_head();?>
