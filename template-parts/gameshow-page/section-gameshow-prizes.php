5e7a8b7adb55433c843f7952c70e4e6162db6673
Slip2188 <81004201+Slip2188@users.noreply.github.com>
2022-02-17 17:56:22 +0530

    Small Gameshow Page Changes Part 2

--- template-parts/gameshow-page/section-gameshow-prizes.php
+++ template-parts/gameshow-page/section-gameshow-prizes.php
@@ -24,7 +24,7 @@ $doc = new DOMXPath( $doc );
             ?>
         </div>
 
-        <div class="row" style="padding-top: 100px;">
+        <div class="row">
             <div class="col-md-6">
                 <img src="<?php echo get_template_directory_uri();?>/assets/images/gameshow-trophy.svg" width="100%" style="margin-bottom: 10%;">
             </div>
