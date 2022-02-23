48daddbf292827930913733d74c84e097621fd39
Bhavye Mathur <bhavyemathur@gmail.com>
2022-02-23 20:39:25 +0530

    CFR Links Page
    Added a CFR links page (/links) to replace the older CFR linktree.
    
    Configure a "CFR links" page on WP. Needs to be made dynamic.

---
+++ templates/template-cfrlinks.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Template Name: CFR Links
+*/
+?>
+
+<?php get_header();?>
+
+<?php get_template_part( "template-parts/section", "cfr-social-media" );?>
+<?php get_template_part( "template-parts/home-page/section", "instawall" );?>
+
+<?php get_footer();?>
